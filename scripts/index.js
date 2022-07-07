6b62d99cd486ac0f339b51302b6c64214f28924f
Kighte <kighte04@gmail.com>
2022-07-07 20:04:35 +0200

    A lot of changes

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,16 @@
+alert('Atención: esta página sigue en desarrollo, por lo tanto está incompleta y puede presentar errores.')
+
 // Variables linked to the DOM.
+
+let viewport = document.getElementById("viewport");
 
 let navBar = document.querySelector(".nav");
 let navBarLinks = document.querySelectorAll(".nav-links");
 
 let coverTitleContainer = document.querySelector(".cover-proyect-title-container");
 let coverTitle = document.querySelector(".cover-proyect-title");
-
 let aboutProyectTextContainer = document.querySelector(".about-proyect-text-container");
 let aboutProyectText = document.querySelector(".about-proyect-text");
-
 let charactersDisplayContainer = document.querySelector(".characters-display-container");
 let charactersDisplay = document.querySelectorAll(".characters-display");
 let charactersTextGlobal = document.querySelectorAll(".characters-text");
@@ -17,14 +19,19 @@ let charactersButton = document.querySelectorAll(".characters-button");
 let charactersButtonString = document.querySelectorAll(".characters-button-string");
 let charactersButtonImg = document.querySelectorAll(".characters-button-img");
 
-let secondArticle = document.querySelector(".second-article");
+let secondArticle = document.getElementById("second-article");
 let muñecoPesao = document.getElementById("muñeco-pesao");
 let muñecoPesaoImg = document.querySelector(".muñeco-pesao-img");
+let muñecoPesaoDiálogo = document.getElementById("muñeco-pesao-diálogo");
 let ediciónPlate = document.querySelectorAll(".edición-plate");
 let ediciónPlateBox = document.querySelectorAll(".edición-plate-2-1");
 let ediciónPlateImg = document.querySelectorAll(".edición-plate-1-1-img");
 let ediciónPlateBorder = document.querySelectorAll(".edición-plate-1-2-border");
 let ediciónPlateText = document.querySelectorAll("edición-plate-1-2-text-container");
+
+let thirtyArticle = document.getElementById("thirty-article");
+let galeríaIntroductionTitle = document.querySelector(".galería-introduction-title-h2");
+let galeríaImg = document.querySelectorAll(".galería-img");
 
 const multipleNodeColorConverter = (varName,color)=>{
 	for (let i=0; i<varName.length; i++) {
@@ -58,28 +65,27 @@ document.addEventListener("scroll", ()=>{
 	if (rectCharPlate.top <= 420 && rectCharPlate.top >= 48) {
 		displayCharactersPlates();
 	}
-	ediciónAnimation(0);
-	ediciónAnimation(1);
-	ediciónAnimation(2);
+	edicionesAnimation(0);
+	edicionesAnimation(1);
+	edicionesAnimation(2);
 	let rectSecondArticle = secondArticle.getBoundingClientRect();
-	if (rectSecondArticle.top <= 226 && rectSecondArticle.top >= (-1076)) {
+	if (rectSecondArticle.top <= 226 && rectSecondArticle.top >= (-1313)) {
+		muñecoPesao.style.animation = "muñecoPesaoDisplay 1s forwards";
+		muñecoPesao.style.display = "inline";
+		muñecoPesao.style.opacity = "1";
+		muñecoPesaoImg.style.transform = "rotate(30deg)";
+		setTimeout(muñecoPesaoDiálogoDisplay, 500)
+	} else if (rectSecondArticle.top != 226 && rectSecondArticle.top != (-1076)) {
+		muñecoPesao.style.animation = "muñecoPesaoHide 1s forwards";
+		muñecoPesao.style.opacity = "1";
+		setTimeout(muñecoPesaoDiálogoHide, 500)
+	}
+	let rectThirtyArticle = thirtyArticle.getBoundingClientRect();
+	console.log(rectThirtyArticle.top);
+	if (rectThirtyArticle.top <= 265 && rectThirtyArticle.top >= -11) {
+		galeríaIntroductionTitle.style.animation = "galeríaIntroductionTitle 1s infinite alternate"
 	}
 })
-
-function ediciónAnimation(wichPlate) {
-	let rectEdiciónPlate = ediciónPlate[wichPlate].getBoundingClientRect();
-	let rectEdiciónPlateBorder = ediciónPlateBorder[wichPlate].getBoundingClientRect();
-	if (rectEdiciónPlate.top <= 371 && rectEdiciónPlate.top >= 61) {
-		ediciónPlateImg[wichPlate].style.animation = "edicionesPlateImgDisplay 1s forwards";
-	}
-	if (rectEdiciónPlate.top <= 371 && rectEdiciónPlate.top >= 61) {
-		ediciónPlateBox[wichPlate].style.animation = "ediciónPlateDisplay 1s forwards"
-		ediciónPlateBox[wichPlate].style.display = "flex";
-	}
-	if (rectEdiciónPlateBorder.top <= 663 && rectEdiciónPlateBorder.top >= 61) {
-		ediciónPlateBorder[wichPlate].style.animation = "edicionesPlateTextDisplay 1s forwards";
-	}
-}
 
 const navBarMinimize = ()=>{
 	navBar.style.position = "fixed"
@@ -148,3 +154,39 @@ charactersButton[1].addEventListener("click", ()=>{
 		hideCharacterInfo(1)
 	}
 })
+
+
+const muñecoPesaoDiálogoDisplay = ()=>{
+	muñecoPesaoDiálogo.style.animation = "muñecoPesaoDiálogoDisplay 1s forwards";
+	muñecoPesaoDiálogo.style.display = "inline";
+	muñecoPesaoDiálogo.style.opacity = "1";
+}
+
+const muñecoPesaoDiálogoHide = ()=>{
+	muñecoPesaoDiálogo.style.animation = "muñecoPesaoDiálogoHide 1s forwards";
+	muñecoPesaoDiálogo.style.opacity = "1";
+}
+
+function edicionesAnimation(wichPlate) {
+	let rectEdiciónPlate = ediciónPlate[wichPlate].getBoundingClientRect();
+	let rectEdiciónPlateBorder = ediciónPlateBorder[wichPlate].getBoundingClientRect();
+	if (rectEdiciónPlate.top <= 371 && rectEdiciónPlate.top >= 61) {
+		ediciónPlateImg[wichPlate].style.animation = "edicionesPlateImgDisplay 1s forwards";
+	}
+	if (rectEdiciónPlate.top <= 371 && rectEdiciónPlate.top >= 61) {
+		ediciónPlateBox[wichPlate].style.animation = "ediciónPlateDisplay 1s forwards"
+		ediciónPlateBox[wichPlate].style.display = "flex";
+	}
+	if (rectEdiciónPlateBorder.top <= 663 && rectEdiciónPlateBorder.top >= 61) {
+		ediciónPlateBorder[wichPlate].style.animation = "edicionesPlateTextDisplay 1s forwards";
+	}
+}
+
+console.log(galeríaImg);
+
+for (i=0;i<galeríaImg.length;i++) {
+	let x = i;
+	galeríaImg[i].addEventListener('click', ()=>{
+		window.open(galeríaImg[x].src);
+	})
+}
