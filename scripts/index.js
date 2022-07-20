2a1ce3348cef6b8c87370698843489677467a9e3
Kighte <kighte04@gmail.com>
2022-07-20 15:05:38 +0200

    responsive advances

--- scripts/index.js
+++ scripts/index.js
@@ -62,7 +62,187 @@ const multipleNodeSizeConverter = (varName,size)=>{
 
 // Event to execute scroll functions on the hole document.
 
-if (screen.width >= 800) {
+if (screen.width >= 1600) {
+
+	for (i = 0; i < navBarLinks.length; i++){
+		navBarLinks[i].style.fontSize = '40px';
+	}
+	document.addEventListener("scroll", ()=>{
+		const scrollY = window.scrollY;
+		if (scrollY !== 0) {
+			navBarMinimize();
+		} else {
+			navBarMaximize();
+		}
+		let rectCoverTitle = coverTitleContainer.getBoundingClientRect();
+		if (rectCoverTitle.top <= 600 && rectCoverTitle.top >= 20) {
+			paimemanTitleFall();
+		}
+		let rectTextDisplay = aboutProyectTextContainer.getBoundingClientRect();
+		if (rectTextDisplay.top <= 450 && rectTextDisplay.top >= -17) {
+			proyectTextOpacity();
+		}
+		let rectCharPlate = charactersDisplayContainer.getBoundingClientRect();
+		if (rectCharPlate.top <= 420 && rectCharPlate.top >= 48) {
+			displayCharactersPlates();
+		}
+		edicionesAnimation(0);
+		edicionesAnimation(1);
+		edicionesAnimation(2);
+		let rectSecondArticle = secondArticle.getBoundingClientRect();
+		if (rectSecondArticle.top <= 226 && rectSecondArticle.bottom >= (200)) {
+			muñecoPesao.style.animation = "muñecoPesaoDisplay 1s forwards";
+			muñecoPesao.style.display = "inline";
+			muñecoPesao.style.opacity = "1";
+			muñecoPesaoImg.style.transform = "rotate(30deg)";
+			setTimeout(muñecoPesaoDiálogoDisplay, 500)
+		} else {
+			muñecoPesao.style.animation = "muñecoPesaoHide 1s forwards";
+			muñecoPesao.style.opacity = "1";
+			setTimeout(()=>{
+			}, 100)
+			setTimeout(muñecoPesaoDiálogoHide, 500)
+		}
+		let rectThirtyArticle = thirdArticle.getBoundingClientRect();
+		if (rectThirtyArticle.top <= 265 && rectThirtyArticle.top >= -11) {
+			galeríaIntroductionTitle.style.animation = "galeríaIntroductionTitle 1s infinite alternate"
+		}
+		let rectWhoamiH2 = whoamiH2.getBoundingClientRect();
+		if (rectWhoamiH2.top <= 368 && rectWhoamiH2.top >= 55) {
+			whoamiH2.style.animation = "whoamiH2Appear 1s forwards";
+		}
+		let rectWhoamiText = whoamiText.getBoundingClientRect();
+		if (rectWhoamiText.top <= 386 && rectWhoamiText.top >= 44) {
+			whoamiText.style.animation = "whoamiTextAppear 1s forwards";
+		}
+	})
+	
+	const navBarMinimize = ()=>{
+		navBar.style.position = "fixed"
+		navBar.style.top = "0";
+		navBar.style.background = "#0005";
+		navBar.style.animation = "navMinimize 1s forwards";
+		multipleNodeSizeConverter(navBarLinks,"30px");
+		multipleNodeColorConverter(navBarLinks,"#fff");
+		
+	}
+	
+	const navBarMaximize = ()=>{
+		navBar.style.position = "initial"
+		navBar.style.background = "#efefef";
+		navBar.style.animation = "navMaximize 1s forwards";
+		multipleNodeSizeConverter(navBarLinks,"40px");
+		multipleNodeColorConverter(navBarLinks,"#000");
+	}
+	
+	const paimemanTitleFall = ()=>{
+		coverTitle.style.animation = "coverTitleFall 0.6s forwards";
+	}
+	
+	const proyectTextOpacity = ()=>{
+		aboutProyectText.style.animation = "aboutProyectTextOpacity 2s forwards";
+	}
+	
+	const displayCharactersPlates = ()=>{
+		charactersDisplay[0].style.animation = "characterPlate1 0.8s forwards";
+		charactersDisplay[0].style.display = "grid";
+		charactersDisplay[1].style.animation = "characterPlate2 0.8s forwards";
+		charactersDisplay[1].style.display = "grid";
+	}
+	
+	// Events to characters plate button interact.
+	
+	
+	const displayCharacterInfo = (wichChar)=>{
+		charactersText[wichChar].style.display = "flex";
+		charactersButtonString[wichChar].innerHTML = "Click para minimizar";
+		charactersButtonImg[wichChar].style.animation = "ImgRotate 1s normal forwards";
+		textNone[wichChar] = false;
+	}
+	
+	const hideCharacterInfo = (wichChar)=>{
+		charactersText[wichChar].style.display = "none";
+		charactersButtonString[wichChar].innerHTML = "Click para maximizar";
+		charactersButtonImg[wichChar].style.animation = "ImgRotate2 1s forwards";
+		textNone[wichChar] = true;
+	}
+	
+	let textNone = [true, true];
+	charactersButton[0].addEventListener("click", ()=>{
+		if (textNone[0] === true) {
+			displayCharacterInfo(0);
+		} else {
+			hideCharacterInfo(0)
+		}
+	})
+	
+	charactersButton[1].addEventListener("click", ()=>{
+		if (textNone[1] === true) {
+			displayCharacterInfo(1);
+		} else {
+			hideCharacterInfo(1)
+		}
+	})
+	
+	
+	const muñecoPesaoDiálogoDisplay = ()=>{
+		muñecoPesaoDiálogo.style.animation = "muñecoPesaoDiálogoDisplay 1s forwards";
+		muñecoPesaoDiálogo.style.display = "inline";
+		muñecoPesaoDiálogo.style.opacity = "1";
+	}
+	
+	const muñecoPesaoDiálogoHide = ()=>{
+		muñecoPesaoDiálogo.style.animation = "muñecoPesaoDiálogoHide 1s forwards";
+		muñecoPesaoDiálogo.style.opacity = "1";
+	}
+	
+	function edicionesAnimation(wichPlate) {
+		let rectEdiciónPlate = ediciónPlate[wichPlate].getBoundingClientRect();
+		let rectEdiciónPlateBorder = ediciónPlateBorder[wichPlate].getBoundingClientRect();
+		if (rectEdiciónPlate.top <= 371 && rectEdiciónPlate.top >= 61) {
+			ediciónPlateImg[wichPlate].style.animation = "edicionesPlateImgDisplay 1s forwards";
+		}
+		if (rectEdiciónPlate.top <= 371 && rectEdiciónPlate.top >= 61) {
+			ediciónPlateBox[wichPlate].style.animation = "ediciónPlateDisplay 1s forwards"
+			ediciónPlateBox[wichPlate].style.display = "flex";
+		}
+		if (rectEdiciónPlateBorder.top <= 663 && rectEdiciónPlateBorder.top >= 61) {
+			ediciónPlateBorder[wichPlate].style.animation = "edicionesPlateTextDisplay 1s forwards";
+		}
+	}
+	
+	for (i=0;i<galeríaImg.length;i++) {
+		let x = i;
+		galeríaImg[i].addEventListener('click', ()=>{
+			window.open(galeríaImg[x].src);
+		})
+	}
+	
+	for (i=0; i<comprasButton.length; i++) {
+		comprasButton[i].addEventListener('click', ()=>{
+			comprasButtonFakeContainer.style.display = 'flex';
+			body.style.height = '100%';
+			body.style.overflowY = 'hidden';
+			html.style.overflowX = 'initial';
+			audioCDC.play();
+		})
+	}
+	
+	comprasButtonFakeQuit.addEventListener('mouseover', ()=>{
+		comprasButtonFakeQuit.style.animation = 'quitHover 1s forwards';
+	})
+	
+	comprasButtonFakeQuit.addEventListener('mouseout', ()=>{
+		comprasButtonFakeQuit.style.animation = 'quitOut 1s forwards';
+	})
+	
+	comprasButtonFakeQuit.addEventListener('click', ()=>{
+		comprasButtonFakeContainer.style.display = 'none';
+		body.style.overflowY = 'initial';
+		html.style.overflowX = 'hidden';
+		audioCDC.pause();
+	})
+}else if (screen.width >= 800) {
 
 	document.addEventListener("scroll", ()=>{
 		const scrollY = window.scrollY;
